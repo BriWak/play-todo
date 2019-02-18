2953c8fc268a0d34d30e6e0a1b26923bfa0e0697
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2019-02-18 14:39:34 +0000

    First Session

---
+++ app/models/Todo.scala
@@ -0,0 +1,22 @@
+package models
+
+import java.time.LocalDate
+
+sealed abstract case class  NonEmptyString(value: String)
+
+object NonEmptyString {
+
+  def apply(value: String): Option[NonEmptyString] =
+    if (value.nonEmpty) Some(new NonEmptyString(value) {})
+    else None
+}
+
+case class Todo(name: NonEmptyString, description: String, state: TodoState)
+
+
+
+sealed trait TodoState
+
+case object Open extends TodoState
+case class Completed(completeOnDate: LocalDate) extends TodoState
+case class Closed(closedOn: LocalDate) extends TodoState
